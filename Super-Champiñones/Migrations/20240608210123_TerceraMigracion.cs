f3ad8981f714aa816754a7864d76c20b8058d6f9
unknown <edson.juarez@sistemas.edu.bo>
2024-06-08 17:02:54 -0400

    Fix:class partido

---
+++ Super-Champiñones/Migrations/20240608210123_TerceraMigracion.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Super_Champiñones.Migrations
+{
+    /// <inheritdoc />
+    public partial class TerceraMigracion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UrlFotoA",
+                table: "Partidos",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UrlFotoB",
+                table: "Partidos",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UrlFotoA",
+                table: "Partidos");
+
+            migrationBuilder.DropColumn(
+                name: "UrlFotoB",
+                table: "Partidos");
+        }
+    }
+}
--- Super-Champiñones/Models/Partido.cs
+++ Super-Champiñones/Models/Partido.cs
@@ -12,6 +12,9 @@ namespace Super_Champiñones.Models
         public string? EquipoB { get; set; }
         public DateTime?Fecha_Hora { get; set; }
         public decimal? Costo{ get; set; }
+        public string? UrlFotoA { get; set; }
+        public string? UrlFotoB { get; set; }
         public virtual List<Venta>? Ventas { get; set; }
+
     }
 }
