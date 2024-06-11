d7a50f4eccb6ca45cae10cc0629a8b8ab04b67da
unknown <edson.juarez@sistemas.edu.bo>
2024-06-10 20:53:12 -0400

    Controladores

---
+++ Super-Champiñones/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Super_Champiñones.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Super-Champiñones/obj/Debug/net8.0/Super-Champiñones.AssemblyInfo.cs
+++ Super-Champiñones/obj/Debug/net8.0/Super-Champiñones.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Super-Champiñones")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c1eb5502b35ed791de22b506e7b2fb92c9ac9b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3ad8981f714aa816754a7864d76c20b8058d6f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Super-Champiñones")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Super-Champiñones")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
